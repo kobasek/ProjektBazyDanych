a7ced309c6ea1288a7c00cb2717516f70692ab2e
Artur Janoś <artur.janos@gmail.com>
2016-04-25 10:39:23 +0200

    update

--- BazyDanych/BazyDanych/Form2.Designer.cs
+++ BazyDanych/BazyDanych/Form2.Designer.cs
@@ -54,7 +54,7 @@
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(828, 408);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Opieka nad pojazdami";
             this.tabPage1.UseVisualStyleBackColor = true;
             this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
             // 
@@ -65,7 +65,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(828, 408);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Zarządzanie pojazdami";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // linkLabel1
