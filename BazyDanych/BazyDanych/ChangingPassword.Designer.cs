a4c6beb40ab4aca700b3543604fede0182bb3acf
Artur Janoś <artur.janos@gmail.com>
2016-04-30 20:52:39 +0200

    Update

--- BazyDanych/BazyDanych/ChangingPassword.Designer.cs
+++ BazyDanych/BazyDanych/ChangingPassword.Designer.cs
@@ -41,7 +41,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Location = new System.Drawing.Point(13, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(90, 13);
             this.label1.TabIndex = 0;
