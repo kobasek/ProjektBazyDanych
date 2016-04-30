aeee76e8311aca959ace8602514590b04ad8dad6
Łukasz <gorzelanczykl@gmail.com>
2016-04-30 23:55:00 +0200

    Update ProfilWindow

--- BazyDanych/BazyDanych/ProfilWindow.cs
+++ BazyDanych/BazyDanych/ProfilWindow.cs
@@ -27,5 +27,10 @@ namespace BazyDanych
         {
             this.Close();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
