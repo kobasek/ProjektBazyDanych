b63866a497b013ce0a0f24c759ce8f6968d45014
Artur Janoś <artur.janos@gmail.com>
2016-05-01 17:37:07 +0200

    Update

--- BazyDanych/BazyDanych/CarDetailsWindow.Designer.cs
+++ BazyDanych/BazyDanych/CarDetailsWindow.Designer.cs
@@ -300,7 +300,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(222, 189);
+            this.button1.Location = new System.Drawing.Point(253, 189);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 63;
@@ -315,6 +315,7 @@
             this.button2.TabIndex = 64;
             this.button2.Text = "Stan techniczny";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // CarDetailsWindow
             // 
