7d9a3acf438bd8cdd41d5b646b67583c86efab80
bialy1994 <michal94b@gmail.com>
2016-09-10 11:47:10 +0200

    Export bazy
    aby poprawnie zaimportować bazę należy:
    - zainstalować mysql
    - stworzyć schemat o nazwie "projekt_bazy_danych"
    - kliknąć import i wybrać folder "Export bazy"

---
+++ BazyDanych/Export bazy/projekt_bazy_danych_uzytkownik.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE  IF NOT EXISTS `projekt_bazy_danych` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `projekt_bazy_danych`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: projekt_bazy_danych
+-- ------------------------------------------------------
+-- Server version	5.7.14-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `uzytkownik`
+--
+
+DROP TABLE IF EXISTS `uzytkownik`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `uzytkownik` (
+  `id` int(11) NOT NULL,
+  `uprawnienia` char(1) NOT NULL,
+  `imie` varchar(20) NOT NULL,
+  `nazwisko` varchar(20) NOT NULL,
+  `adres_zamieszkania` varchar(100) NOT NULL,
+  `miejsce_urodzenia` varchar(20) NOT NULL,
+  `data_urodzenia` date NOT NULL,
+  `plec` char(1) NOT NULL,
+  `nr_dowodu` varchar(9) NOT NULL,
+  `pesel` varchar(11) NOT NULL,
+  `prawo_jazdy` varchar(37) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `haslo` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `uzytkownik`
+--
+
+LOCK TABLES `uzytkownik` WRITE;
+/*!40000 ALTER TABLE `uzytkownik` DISABLE KEYS */;
+/*!40000 ALTER TABLE `uzytkownik` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-09-10 11:41:31
